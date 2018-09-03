18006664d79df0c7e86b5e909f09a091cee156a5
rwon@corp.drillmap.com <rwon@corp.drillmap.com>
2018-09-02 21:28:54 -0500

    add game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
 
-    window.onload = function() {
-        $("#start").on("click", game.start);
-        $("#stop").on("click", game.stop);
-        $("#reset").on("click", game.reset);
-      };
+    window.onload = function () {
+        // $("#start").on("click", game.start);
+        // $("#stop").on("click", game.stop);
+        // $("#reset").on("click", game.reset);
+    };
 
     // This function remove element from an array
     function removeElement(array, element) {
@@ -15,7 +15,39 @@ $(document).ready(function () {
         }
     }
 
-    
+    // Object for Questions
+    function Question(question, choices, answer) {
+        this.question = question;
+        this.choices = choices;
+        this.answer = answer;
+
+        this.displayQuestion = function () {
+            console.log(this.question);
+        };
+
+        this.displayChoices = function () {
+            console.log(this.choices[0]);
+            console.log(this.choices[1]);
+            console.log(this.choices[2]);
+        }
+
+        this.displayAnswer = function () {
+            console.log(this.answer);
+        };
+    }
+
+    function playGame(question, guess) {
+        question.displayQuestion();
+        question.displayChoices();
+        if (guess === question.answer) {
+            score++;
+            console.log("Correct");
+        }
+        else {
+            console.log("Incorrect");
+        }
+    }
+
     // -- main program
 
 
@@ -32,5 +64,22 @@ $(document).ready(function () {
     });
 
 
+
+    let q1 = new Question("What is A", ["A", "B", "C"], "A");
+    let q2 = new Question("What is B", ["A", "B", "C"], "B");
+    let q3 = new Question("What is C", ["A", "B", "C"], "C");
+    let q4 = new Question("What is A", ["A", "B", "C"], "A");
+    let q5 = new Question("What is B", ["A", "B", "C"], "B");
+    let q6 = new Question("What is C", ["A", "B", "C"], "C");
+
+    let questions = [q1, q2, q3, q4];
+    let inputs = ["A", "A", "B", "A"];
+    let score = 0;
+
+    for (let i = 0; i < questions.length; i++) {
+        playGame(questions[i], inputs[i]);
+        console.log(score);
+    }
+    console.log("final score = " + score);
 
 });
